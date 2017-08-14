8272fe11301337a10f5a1feb3d18f903fc58fb84
salonikrishnan <salonikrishnan@epsymac146.psy.ox.ac.uk>
2017-08-14 12:13:34 +0100

    Updated demo trialcode

--- StopSignalCode/demo.m
+++ StopSignalCode/demo.m
@@ -64,29 +64,11 @@ if type==2 || type==3
     recordedaudio = [];
 end
 
+load('democode.mat');
+
 totalcnt=1;  % this is the overall counter
 
 for block=1:NBLOCKS % change number of blocks
-    NUMCHUNKS=1;
-    for  tc=1:NUMCHUNKS
-        for qblock=1:4
-            LadderOrder=[randperm(2) randperm(2)];
-            arrows = [1 1 0 0];
-            [p  rand_idx]=sort(rand(1,4));
-            arrows=arrows(rand_idx);
-            for st=1:4
-                %there are 4 in each, one stop, three go
-                mini = [1 arrows(1) LadderOrder(st); 0 arrows(2) 0; 0 arrows(3) 0; 0 arrows(4) 0;];
-                [p  rand_idx]=sort(rand(1,4));
-                mini=mini(rand_idx,:);
-                start=(tc-1)*64+(qblock-1)*16+(st-1)*4+1;
-                endof=(tc-1)*64+(qblock-1)*16+(st)*4;
-                trialcode(start:endof,:)=mini;
-            end
-        end
-    end
-    
-    
     
     Ladder1=100;
     Ladder2=250;
