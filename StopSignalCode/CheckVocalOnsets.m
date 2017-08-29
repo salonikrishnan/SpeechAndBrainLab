62875fa7595ef32dd7bb1d74d9403dec15f050bd
salonikrishnan <salonikrishnan@gmail.com>
2017-08-29 09:57:11 +0100

    Check Vocal Onsets for fMRI scan

---
+++ StopSignalCode/CheckVocalOnsets.m
@@ -0,0 +1,20 @@
+function CheckVocalOnsets(inputfile,subject_code,block)
+
+if nargin < 3
+    inputfile = input('File name','s');
+    subject_code = input('Subject code','s');
+    block = input('Scan number:');
+end
+
+load(inputfile);
+cd voicefiles/subject_code
+files = dir(strcat('block',num2str(block),'*'))
+
+for i = 1:length(Seeker); 
+    
+    if Seeker(i,9)~=0; Seeker(i,7) = 1; end; 
+
+end
+
+save(outfile, 'Seeker');
+end
